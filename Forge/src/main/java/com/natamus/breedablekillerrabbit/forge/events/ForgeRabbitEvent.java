d41a6593ef2ae437bfa5e15fec4923e933b6b8a6
Rick South <rick@natamus.com>
2023-12-12 10:46:24 +0100

    Breedable Killer Rabbit source code for 1.18.2.

---
+++ Forge/src/main/java/com/natamus/breedablekillerrabbit/forge/events/ForgeRabbitEvent.java
@@ -0,0 +1,65 @@
+package com.natamus.breedablekillerrabbit.forge.events;
+
+import com.natamus.breedablekillerrabbit.events.RabbitEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.BabyEntitySpawnEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeRabbitEvent {
+	@SubscribeEvent
+	public void onBaby(BabyEntitySpawnEvent e) {
+		AgeableMob child = e.getChild();
+		Level level = child.getCommandSenderWorld();
+		if (level.isClientSide) {
+			return;
+		}
+
+		Mob parentA = e.getParentA();
+		Mob parentB = e.getParentB();
+		if (!(parentA instanceof Animal) || !(parentB instanceof Animal)) {
+			return;
+		}
+
+		RabbitEvent.onBaby((ServerLevel)level, (Animal)parentA, (Animal)parentB, e.getChild());
+	}
+	
+	@SubscribeEvent
+	public void onEntityInteract(PlayerInteractEvent.EntityInteract e) {
+		Level world = e.getWorld();
+		if (world.isClientSide) {
+			return;
+		}
+
+		RabbitEvent.onEntityInteract(e.getPlayer(), e.getWorld(), e.getHand(), e.getTarget(), null);
+	}
+	
+	@SubscribeEvent
+	public void onTarget(LivingAttackEvent e) {
+		Entity entity = e.getEntity();
+		if (!RabbitEvent.onTarget(entity.level, entity, e.getSource(), e.getAmount())) {
+			e.setCanceled(true);
+		}
+	}
+	
+	@SubscribeEvent
+	public void mobSpawn(EntityJoinWorldEvent e) {
+		RabbitEvent.mobSpawn(e.getWorld(), e.getEntity());
+	}
+	
+	@SubscribeEvent
+	public void onPlayerDamage(LivingHurtEvent e) {
+		Entity entity = e.getEntity();
+		RabbitEvent.onPlayerDamage(entity.level, entity, e.getSource(), e.getAmount());
+	}
+}
