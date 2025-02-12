5b63a02798b990b19aad0d415fdfee1b1edc2a2d
Rick South <rick@natamus.com>
2025-02-12 23:42:14 +0100

    (v3.8) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/breedablekillerrabbit/ModForge.java
+++ Forge/src/main/java/com/natamus/breedablekillerrabbit/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeRabbitEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeRabbitEvent.class);
 	}
 
 	private static void setGlobalConstants() {
--- NeoForge/src/main/java/com/natamus/breedablekillerrabbit/neoforge/events/NeoForgeRabbitEvent.java
+++ NeoForge/src/main/java/com/natamus/breedablekillerrabbit/neoforge/events/NeoForgeRabbitEvent.java
@@ -8,14 +8,12 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.living.BabyEntitySpawnEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
-@EventBusSubscriber
 public class NeoForgeRabbitEvent {
 	@SubscribeEvent
 	public static void onBaby(BabyEntitySpawnEvent e) {
