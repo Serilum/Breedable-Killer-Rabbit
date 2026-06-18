ec88b6a703471e3495d1d27a6540b0a44a92cc2e
Rick South <rick@natamus.com>
2026-06-19 00:08:32 +0200

    Updated Breedable Killer Rabbit to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/breedablekillerrabbit/util/Reference.java
+++ Common/src/main/java/com/natamus/breedablekillerrabbit/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "breedablekillerrabbit";
 	public static final String NAME = "Breedable Killer Rabbit";
 	public static final String VERSION = "4.0";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
